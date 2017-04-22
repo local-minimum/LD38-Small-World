14eda106b6275efbaf25e62615cb4a702038cb85
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-22 14:27:08 +0200

    First basic minigame files

---
+++ Assets/Scripts/MG_Asteroid/PlayerSpacecraft.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerSpacecraft : MonoBehaviour
+{
+    public float m_Speed = 5.0f;
+    public float m_RotationSpeed = 100.0F;
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * m_Speed;
+        float rotation = Input.GetAxis("Horizontal") * m_RotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(translation, 0, 0);
+        transform.Rotate(0, 0, rotation);
+    }
+}
+
