75b735effdb6d9f7c544557d145362864e4ea51c
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-22 12:01:25 +0200

    Made singleton

--- Assets/Scripts/MiniGameLoader.cs
+++ Assets/Scripts/MiniGameLoader.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using LocalMinimum;
 
-public class MiniGameLoader : MonoBehaviour {
+public class MiniGameLoader : Singleton<MiniGameLoader>{
 
     [SerializeField]
     string[] SceneNames;
