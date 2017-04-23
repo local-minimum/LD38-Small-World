d62bcc29251d597e4e84ecc5bc8b67915e9f26cf
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-23 09:11:31 +0200

    pub and mini game

---
+++ Assets/Scripts/MG_Burp/MG_BurpItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_BurpItem : MonoBehaviour {
+
+    public float maxSpeed = 2;
+    public float minSpeed = 1;
+    Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	
+	void Update () {
+        Vector2 vel = rb.velocity;
+        rb.velocity = vel.normalized * Mathf.Clamp(vel.magnitude, minSpeed, maxSpeed);
+	}
+}
---
+++ Assets/Scripts/MG_Burp/MG_BurpWind.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MG_BurpWind : MonoBehaviour {
+
+    public float force = 10f;
+    public bool burping = true;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.GetComponent<MG_BurpItem>() != null)
+        {
+            Vector3 forceV = transform.up * force;
+            other.GetComponent<Rigidbody2D>().AddForce(forceV, ForceMode2D.Force);
+        }
+    }
+}
