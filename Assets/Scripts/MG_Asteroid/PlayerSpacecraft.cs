a025ad5ccb6c906fbe5b05a1918dc38506991bb9
higipha <higipha@hotmail.com>
2017-04-23 11:54:42 +0200

    Fixed player movement, beginning to ad conversaionpieces to pick up.

--- Assets/Scripts/MG_Asteroid/PlayerSpacecraft.cs
+++ Assets/Scripts/MG_Asteroid/PlayerSpacecraft.cs
@@ -2,22 +2,30 @@
 
 public class PlayerSpacecraft : MonoBehaviour
 {
-    public float m_Speed = 5.0f;
-    public float m_RotationSpeed = 100.0F;
-    
-    void Update()
-    {
-        float translation = Input.GetAxis("Vertical") * m_Speed;
-        float rotation = Input.GetAxis("Horizontal") * m_RotationSpeed;
+    public float m_AccelerationForce = 100.0f;
+    public float m_RotationForce = 0.1F;
 
-        if (translation > 0)
+    private Rigidbody2D m_RigidBody;
+
+    private void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float rotation = -Input.GetAxis("Horizontal");
+        m_RigidBody.AddTorque(rotation * m_RotationForce);
+        float acceleration = Input.GetAxis("Vertical");
+        m_RigidBody.AddForce(transform.right * acceleration * m_AccelerationForce);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Conversationoid")
         {
-            translation *= Time.deltaTime;
-            transform.Translate(translation, 0, 0);
+            //MGAsteroidPlayer.PlayerGotConversationoid(collision.gameObject.GetComponent<ConversationPiece>());   
         }
-        
-        rotation *= Time.deltaTime;
-        transform.Rotate(0, 0, rotation);
     }
 }
 
