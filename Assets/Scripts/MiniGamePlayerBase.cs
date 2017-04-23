873723c01adcfc62da4e5e93ebd89976aa3dbb81
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-23 10:43:16 +0200

    Tweaks

--- Assets/Scripts/MiniGamePlayerBase.cs
+++ Assets/Scripts/MiniGamePlayerBase.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LocalMinimum;
 
-public class MiniGamePlayerBase : MonoBehaviour {
+public class MiniGamePlayerBase : Singleton<MiniGamePlayerBase> {
 
     virtual public void Play(int difficulty) { }
 }
