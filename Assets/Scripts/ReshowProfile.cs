429ae7bfab9a1ba48d9c78baf8f1beb1f398192d
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-24 10:49:02 +0200

    Reshow info

---
+++ Assets/Scripts/ReshowProfile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReshowProfile : MonoBehaviour {
+
+    [SerializeField]
+    float reshowDuration = 10;
+
+    bool manualShowing = false;
+
+    public void Activate()
+    {
+
+        if (!manualShowing && !ProfileViewer.instance.ShowingProfile)
+        {
+            ProfileViewer.instance.ShowProfile(Room.instance.Conversation.currentPerson);
+            StartCoroutine(DelayClose());
+        }
+    }
+
+    IEnumerator<WaitForSeconds> DelayClose()
+    {
+        manualShowing = true;
+        yield return new WaitForSeconds(reshowDuration);
+        if (!ProfileViewer.instance.ShowingProfile)
+        {
+            ProfileViewer.instance.HideProfile();
+        }
+        manualShowing = false;
+    }
+}
